85c7de6890f6dfdb4783383fd680e70caed6aec4
Riri <riri.zamor@gmail.com>
2020-11-12 12:23:10 +0100

    quest over

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import { render } from "react-dom"
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from "@apollo/client"
+import "./index.css"
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache(),
+})
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function FindLaunches() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      launches(limit: 5) {
+        launch_date_utc
+        launch_success
+        rocket {
+          rocket_name
+        }
+        links {
+          video_link
+        }
+        details
+        id
+      }
+    }
+  `)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  return data.launches.map(data => (
+    <div class="card" key={data.id}>
+      <br />
+      {console.log(data)}
+      <p>{data.launch_date_utc}</p>
+      <p>{data.rocket.rocket_name}</p>
+      <p>{data.details}</p>
+    </div>
+  ))
+}
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+        <FindLaunches />
+      </div>
+    </ApolloProvider>
+  )
+}
+
+render(<App />, document.getElementById("root"))
